682a6ffa487f21a4054dec94cbce2f76871c6a2d
Crowdbotics <team@crowdbotics.com>
2019-05-07 17:52:24 +0000

    initial commit

---
+++ android/app/src/main/java/com/sofrito_la_vieja_3045/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sofrito_la_vieja_3045;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "sofrito_la_vieja_3045";
+    }
+}
